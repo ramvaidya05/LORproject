e0938a7661c161d4b4be79950ab2c499edd40310
ramvaidya05 <72759841+ramvaidya05@users.noreply.github.com>
2024-12-08 21:52:29 -0600

    Update project_flask.py

--- project_flask.py
+++ project_flask.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from flask import Flask, request, render_template_string
 from sklearn.cluster import KMeans
